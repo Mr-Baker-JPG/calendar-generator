22ce8ae2c0dba6eb267c1d87bf447b95a8850be4
Craig Baker <cbaker@jpgacademy.org>
2021-04-10 13:16:28 -0500

    updated coverage folder location

--- jest.config.js
+++ jest.config.js
@@ -15,7 +15,7 @@ export default {
     __dirname,
     path.join(__dirname, "../src"),
   ],
-  coverageDirectory: path.join(__dirname, "../coverage"),
+  coverageDirectory: path.join(__dirname, "/coverage"),
   collectCoverageFrom: ["**/src/**/*.*js"],
   coveragePathIgnorePatterns: [".*/__tests__/.*"],
   watchPlugins: [],
